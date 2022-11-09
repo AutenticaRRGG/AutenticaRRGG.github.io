1065e1f7d809d15281dd8b7e9c5884cc05cc70ff
AutenticaRRGG <117795772+AutenticaRRGG@users.noreply.github.com>
2022-11-08 22:21:03 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
-    authDomain: "pauten43.firebaseapp.com",
-    projectId: "pauten43",
-    storageBucket: "pauten43.appspot.com",
-    messagingSenderId: "167024006136",
-    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
-    measurementId: "G-S4KD63PHHW"
+    apiKey: "AIzaSyDrBod-iaK4LvRrKmLY1gLHfblVIl_AYro",
+  authDomain: "autenticarrgg-faedd.firebaseapp.com",
+  projectId: "autenticarrgg-faedd",
+  storageBucket: "autenticarrgg-faedd.appspot.com",
+  messagingSenderId: "91147361607",
+  appId: "1:91147361607:web:fcd24902a6fe6213448062",
+  measurementId: "G-55HKQQVXPE"
     });
